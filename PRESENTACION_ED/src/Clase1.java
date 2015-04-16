674fd02262e782d180bdec94344957bed36c2abd
cesinolu <cesinolu@gmail.com>
2015-04-16 13:00:08 +0200

    3.0 final

--- PRESENTACION_ED/src/Clase1.java
+++ PRESENTACION_ED/src/Clase1.java
@@ -2,7 +2,7 @@
 public class Clase1 {
 	public static void main(String[] args) {
 		System.out.println("Hola");
-		System.out.println("Conversacion de besugos 2.0");
+		System.out.println("Conversacion de besugos 3.0 final");
 		System.out.println("Adiós");
 	}
 }
