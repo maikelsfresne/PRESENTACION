79afb1c194fae831b2b51de0db7fcac8450ff691
cesinolu <cesinolu@gmail.com>
2015-04-16 12:47:04 +0200

    Añadida conversacion

--- PRESENTACION_ED/src/Clase1.java
+++ PRESENTACION_ED/src/Clase1.java
@@ -2,6 +2,7 @@
 public class Clase1 {
 	public static void main(String[] args) {
 		System.out.println("Hola");
+		System.out.println("Conversacion de besugos");
 		System.out.println("Adiós");
 	}
 }
