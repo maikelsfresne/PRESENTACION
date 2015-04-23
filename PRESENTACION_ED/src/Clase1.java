f7c9fac6a2738c3581594f661978af6963f6cb60
maikelsfresne <maikelsvivaundertaker@hotmail.com>
2015-04-23 13:23:44 +0200

    commit nuevo

--- PRESENTACION_ED/src/Clase1.java
+++ PRESENTACION_ED/src/Clase1.java
@@ -2,7 +2,6 @@
 public class Clase1 {
 	public static void main(String[] args) {
 		System.out.println("Hola");
-		System.out.println("Conversacion de besugos 2.0");
 		System.out.println("Adiós");
 	}
 }
